fc1ec44f0429f4c38899095ddc442005e3842e3e
shuati <ouyangyly@gmail.com>
2023-05-31 20:51:19 +0100

    update the ajax request using promise based ajax

--- javascript/login&register.js
+++ javascript/login&register.js
@@ -1,40 +1,84 @@
-function $(selector){
+function $(selector) {
   return document.querySelector(selector);
 }
-function $$(){
+function $$() {
   return document.querySelectorAll(selector);
 }
 
-// get username and password from login form;
 
-// use ajax to send a psot request to the login.php server;
-var loginForm = $("#login");
-loginForm.addEventListener('submit',function(e){
-  // prevent default refresh;
-  e.preventDefault();
-  let username = $("#login input[name=username]").value;
-  let password = $("#login input[name=password]").value;
-  console.log(username);
-  console.log(password);
-  // set and send post request;
-  let xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("POST","./server/login.php");
-  xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-  console.log("xmlhttp create sucessful");
-  xmlhttp.onreadystatechange = function(){
-    if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
-      let responseObject = JSON.parse(xmlhttp.responseText);
-      if(responseObject.backSuccessful == 1){
-        $(".model-login .error-content").innerText = `welcome: ${username}`;
-        console.log(window.location.pathname);
-        setTimeout(window.location.assign(`./query.php?${username}`),2000);
+// create an ajaxFn function for send the ajax request it return a Promise object.
+// method is the require method;
+// url is the require url;
+// info is the string using in XMLHttpRequest send() method;
+function ajaxFn(method, url, info) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send(info);
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4) {
+        if (xhr.status >= 200 && xhr.status < 300) {
+          resolve(xhr.responseText);
+        } else {
+          reject(error);
+        }
       }
-      if(responseObject.backError == 1){
-        $(".model-login .error-content").innerText = `login failed, please use the correct username and password`;
-      }
-      console.log(responseObject);
     }
-  }
+  })
+}
 
-  xmlhttp.send(`username=${username}&password=${password}&change=0`);
+
+// use ajax to send a post request to the login.php server;
+const loginForm = $("#login");
+loginForm.addEventListener('submit', async function (e) {
+
+  e.preventDefault();
+
+  // get username and password from login form;
+  const username = $("#login input[name=username]").value;
+  const password = $("#login input[name=password]").value;
+  const userInfo = `username=${username}&password=${password}&change=0`
+
+  // send request and get response from server;
+  let responseData = await ajaxFn('POST', './server/login.php', userInfo);
+
+  // parse data and render page;
+  let responseObject = JSON.parse(responseData);
+  if (responseObject.backSuccessful == 1) {
+    $(".model-login .error-content").innerText = `welcome: ${username}`;
+    console.log(window.location.pathname);
+    setTimeout(() => {
+      window.location.assign(`./query.php?${username}`);
+    }, 1000);
+  }
+  if (responseObject.backError == 1) {
+    $(".model-login .error-content").innerText = `login failed, please use the correct username and password`;
+  }
 })
+
+
+//   const userInfo = `username=${username}&password=${password}&change=0`
+//   // set and send post request;
+//   let responseData = await ajaxFn('POST', './server/login.php', userInfo);
+
+//   let xhr = new XMLHttpRequest();
+//   xmlhttp.open("POST", "./server/login.php");
+//   xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+//   console.log("xmlhttp create sucessful");
+//   xmlhttp.onreadystatechange = function () {
+//     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+//       let responseObject = JSON.parse(xmlhttp.responseText);
+//       if (responseObject.backSuccessful == 1) {
+//         $(".model-login .error-content").innerText = `welcome: ${username}`;
+//         console.log(window.location.pathname);
+//         setTimeout(window.location.assign(`./query.php?${username}`), 2000);
+//       }
+//       if (responseObject.backError == 1) {
+//         $(".model-login .error-content").innerText = `login failed, please use the correct username and password`;
+//       }
+//       console.log(responseObject);
+//     }
+//   }
+//   xmlhttp.send(`username=${username}&password=${password}&change=0`);
+// })
--- javascript/query.js
+++ javascript/query.js
@@ -1,8 +1,31 @@
-function $(selector){
+// create two shortcut function for element selection;
+function $(selector) {
   return document.querySelector(selector);
 }
-function $$(){
+function $$() {
   return document.querySelectorAll(selector);
+}
+
+// create an ajaxFn function for send the ajax request it return a Promise object.
+// method is the require method;
+// url is the require url;
+// info is the string using in XMLHttpRequest send() method;
+function ajaxFn(method, url, info) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send(info);
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4) {
+        if (xhr.status >= 200 && xhr.status < 300) {
+          resolve(xhr.responseText);
+        } else {
+          reject(xhr.onerror);
+        }
+      }
+    }
+  })
 }
 
 // show the username in the welcome page.
@@ -11,89 +34,85 @@ let loginUser = pageHref.split("?")[1];
 // login user is a officer or administrator
 var createOfficerButton = $("button.createOfficer");
 var signupForm = $("form[name=signup]");
-if(loginUser == "Daniels"){
+if (loginUser == "Daniels") {
   //user is administrator, replace the welcome words
   $("header.clear-float h1.verifiedUser").innerText = `Administrator: ${loginUser}`;
   // show the create officer button
   createOfficerButton.classList.add("active");
   createOfficerButton.classList.remove("hidden");
   //click the create button show the create Officer form;
-  createOfficerButton.addEventListener("click",e=>{
-    if(signupForm.classList.contains("hidden")){
-    
+  createOfficerButton.addEventListener("click", e => {
+    if (signupForm.classList.contains("hidden")) {
+
       signupForm.classList.remove("hidden");
       signupForm.classList.add("active");
-    }else if(signupForm.classList.contains("active")){
+    } else if (signupForm.classList.contains("active")) {
       signupForm.classList.add("hidden");
       signupForm.classList.remove("active");
     }
   })
-}else{
-  createOfficerButton.addEventListener("click",e=>{
+} else {
+  createOfficerButton.addEventListener("click", e => {
     $("span.errorTips").innerText = "You are not able to create Officer, only Administrator can";
   })
   $("header.clear-float h1.verifiedUser").innerText = `Officer: ${loginUser}`;
 }
 // create new Officer
-signupForm.addEventListener('submit',function(e){
+signupForm.addEventListener('submit', function (e) {
   // prevent default refresh;
   e.preventDefault();
-  let signupUsername = $("form[name=signup] input[name=username]").value;
-  let signupPassword = $("form[name=signup] input[name=password]").value;
-  let cnpassword = $("form[name=signup] input[name=cnpassword]").value;
-  console.log(signupUsername);
-  console.log(signupPassword);
-  console.log(cnpassword);
-  if(signupPassword == cnpassword){
-      // set and send post request;
-    let xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("POST","./server/signup.php");
-    xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-    console.log("xmlhttp create sucessful");
-    xmlhttp.onreadystatechange = function(){
-      if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
-        $("span.errorTips").innerText = xmlhttp.responseText;
-        setTimeout(()=>{window.location.reload()},3000)
-      }
-    }
-    xmlhttp.send(`username=${signupUsername}&cnpassword=${cnpassword}`);
+  const signupUsername = $("form[name=signup] input[name=username]").value;
+  const signupPassword = $("form[name=signup] input[name=password]").value;
+  const cnpassword = $("form[name=signup] input[name=cnpassword]").value;
+  // console.log(signupUsername);
+  // console.log(signupPassword);
+  // console.log(cnpassword);
+  if (signupPassword == cnpassword) {
+    // send request to server and render page;
+    const userInfo = `username=${signupUsername}&cnpassword=${cnpassword}`
+    ajaxFn("POST", "./server/signup.php", userInfo)
+      .then(value => {
+        $("span.errorTips").innerText = value;
+        setTimeout(() => { window.location.reload() }, 1000);
+      }, reason => {
+        $("span.errorTips").innerText = reason;
+      })
   }
- 
-}) 
+})
 
 // change password click the change password button then the form will display
 let changeButton = $("button.changePassword");
 let formChangePassword = $("form[name=changePassword]");
-changeButton.addEventListener("click", e=>{
-  if(formChangePassword.classList.contains("hidden")){
-    
+changeButton.addEventListener("click", e => {
+  if (formChangePassword.classList.contains("hidden")) {
+
     formChangePassword.classList.remove("hidden");
     formChangePassword.classList.add("active");
-  }else if(formChangePassword.classList.contains("active")){
+  } else if (formChangePassword.classList.contains("active")) {
     formChangePassword.classList.add("hidden");
     formChangePassword.classList.remove("active");
   }
-  
+
 })
 // set the username unchangeable
 changepasswordUser.value = loginUser;
 
-formChangePassword.addEventListener("submit",e=>{
+formChangePassword.addEventListener("submit", e => {
   e.preventDefault();
   let changepassword_User = changepasswordUser.value;
   let changepassword_oldPassword = $("input[name=oldpassword]").value;
   let changepassword_newPassword = $("input[name=newpassword]").value;
   let changepassword_cnnewPassword = $("input[name=confirmnewpassword]").value;
-  if(!(changepassword_newPassword == changepassword_cnnewPassword)){
+  if (!(changepassword_newPassword == changepassword_cnnewPassword)) {
     $("span.errorTips").innerText = "two new password not same";
-  }else{
+  } else {
     let xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("POST","./server/login.php");
-    xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-    xmlhttp.onreadystatechange = function(){
-      if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
-        $("span.errorTips").innerText = xmlhttp.responseText;   
-        setTimeout(()=>{window.location.reload()},5000);
+    xmlhttp.open("POST", "./server/login.php");
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.onreadystatechange = function () {
+      if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        $("span.errorTips").innerText = xmlhttp.responseText;
+        setTimeout(() => { window.location.reload() }, 5000);
       }
     }
     xmlhttp.send(`change=1&changeUser=${changepassword_User}&changeOldpassword=${changepassword_oldPassword}&changeNewpassword=${changepassword_cnnewPassword}`);
@@ -104,15 +123,15 @@ formChangePassword.addEventListener("submit",e=>{
 // query the incident and list the incident information;
 let divListIncident = $("div.listIncident");
 let queryIncidentForm = $("form[name=findIncident]");
-queryIncidentForm.addEventListener("submit",e=>{
+queryIncidentForm.addEventListener("submit", e => {
   e.preventDefault();
   let queryIncidentID = $("form[name=findIncident] input[name=incidentID]").value;
-  if(queryIncidentID){
+  if (queryIncidentID) {
     let xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("POST","./server/incident.php");
-    xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-    xmlhttp.onreadystatechange = function(){
-      if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+    xmlhttp.open("POST", "./server/incident.php");
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.onreadystatechange = function () {
+      if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
         divListIncident.innerHTML = xmlhttp.responseText;
       }
     }
@@ -122,11 +141,11 @@ queryIncidentForm.addEventListener("submit",e=>{
 // create new incident
 // get the information from database;
 let divIncident = $("div.incident");
-divIncident.addEventListener("mouseenter", e=>{
+divIncident.addEventListener("mouseenter", e => {
   let xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("GET","./server/incident.php");
-  xmlhttp.onreadystatechange = function(){
-    if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+  xmlhttp.open("GET", "./server/incident.php");
+  xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
       // show all the people ID in the select box;
       let peopleID = $("select[name=incidentPeopleID]");
       let peopleArray = JSON.parse(xmlhttp.responseText).people_ID;
@@ -144,7 +163,7 @@ divIncident.addEventListener("mouseenter", e=>{
         vehicleContent = vehicleArray[index] + vehicleContent;
       }
       vehicleID.innerHTML = vehicleContent;
-      
+
       // show all the vehicle ID in the select box;
       let offenceID = $("select[name=incidentOffenceID]");
       let offenceArray = JSON.parse(xmlhttp.responseText).offence_ID;
@@ -153,53 +172,53 @@ divIncident.addEventListener("mouseenter", e=>{
         offenceContent = offenceArray[index] + offenceContent;
       }
       offenceID.innerHTML = offenceContent;
-}
+    }
   }
   xmlhttp.send();
 })
 
 // create incident 
 let createIncidentForm = $("form[name=createIncident]");
-  createIncidentForm.addEventListener("submit",e=>{
-    e.preventDefault();
-    let createIncidentVehicleID = $("select[name=incidentVehicleID]").value;
-    let createIncidentPeopleID = $("select[name=incidentPeopleID]").value;
-    let createIncidentOffenceID = $("select[name=incidentOffenceID]").value;
-    let createIncidentStatement = $("input[name=incidentReport]").value;
-    let createIncidentDate = $("input[name=incidentDate]").value;
-    if(!(createIncidentPeopleID || createIncidentVehicleID || createIncidentOffenceID || createIncidentStatement ||createIncidentDate)){
-      console.log("please fulfill all the table");
-    }else{
-      let xmlhttp = new XMLHttpRequest();
-      xmlhttp.open("POST","./server/incident.php");
-      xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-      xmlhttp.onreadystatechange = function(){
-        if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
-          incidentCreateFeedback.innerHTML = xmlhttp.responseText;
-        }
+createIncidentForm.addEventListener("submit", e => {
+  e.preventDefault();
+  let createIncidentVehicleID = $("select[name=incidentVehicleID]").value;
+  let createIncidentPeopleID = $("select[name=incidentPeopleID]").value;
+  let createIncidentOffenceID = $("select[name=incidentOffenceID]").value;
+  let createIncidentStatement = $("input[name=incidentReport]").value;
+  let createIncidentDate = $("input[name=incidentDate]").value;
+  if (!(createIncidentPeopleID || createIncidentVehicleID || createIncidentOffenceID || createIncidentStatement || createIncidentDate)) {
+    console.log("please fulfill all the table");
+  } else {
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("POST", "./server/incident.php");
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.onreadystatechange = function () {
+      if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        incidentCreateFeedback.innerHTML = xmlhttp.responseText;
       }
-      xmlhttp.send(`createIncident=1&createIncidentVehicleID=${createIncidentVehicleID}&createIncidentPeopleID=${createIncidentPeopleID}&createIncidentOffenceID=${createIncidentOffenceID}&createIncidentDate=${createIncidentDate}&createIncidentStatement=${createIncidentStatement}`);
     }
-  })
+    xmlhttp.send(`createIncident=1&createIncidentVehicleID=${createIncidentVehicleID}&createIncidentPeopleID=${createIncidentPeopleID}&createIncidentOffenceID=${createIncidentOffenceID}&createIncidentDate=${createIncidentDate}&createIncidentStatement=${createIncidentStatement}`);
+  }
+})
 
 
 // show createPeople form
 let createPeopleForm = $("form[name=createPeople]");
 let queryForm = $("form[name=findPeople]");
-queryForm.addEventListener('submit',function(e){
+queryForm.addEventListener('submit', function (e) {
   // prevent default refresh;
   e.preventDefault();
   let name = $("form[name=findPeople] input[name=name]").value;
   let licenseNumber = $("form[name=findPeople] input[name=licenseNumber]").value;
   console.log(name);
   console.log(licenseNumber);
-    // set and send post request;
+  // set and send post request;
   let xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("POST","./server/queryPeople.php");
-  xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+  xmlhttp.open("POST", "./server/queryPeople.php");
+  xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   console.log("xmlhttp create sucessful");
-  xmlhttp.onreadystatechange = function(){
-    if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+  xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
       let table = document.createElement("table");
       table.className = "pelpleTable";
       table.innerHTML = xmlhttp.responseText;
@@ -207,22 +226,22 @@ queryForm.addEventListener('submit',function(e){
     }
   }
   xmlhttp.send(`name=${name}&licenseNumber=${licenseNumber}`);
-}) 
+})
 
 // queryVehicles;
 let queryVehicles = $("form[name=findVehicles]");
-queryVehicles.addEventListener('submit',function(e){
+queryVehicles.addEventListener('submit', function (e) {
   // prevent default refresh;
   e.preventDefault();
   let vehicleID = $("form[name=findVehicles] input[name=vehicelID]").value;
   console.log(vehicleID);
-    // set and send post request;
+  // set and send post request;
   let xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("POST","./server/queryVehicles.php");
-  xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+  xmlhttp.open("POST", "./server/queryVehicles.php");
+  xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   console.log("xmlhttp create sucessful");
-  xmlhttp.onreadystatechange = function(){
-    if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+  xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
       let content = document.createElement("table");
       content.className = "contentTable";
       content.innerHTML = xmlhttp.responseText;
@@ -230,34 +249,34 @@ queryVehicles.addEventListener('submit',function(e){
     }
   }
   xmlhttp.send(`vehicleID=${vehicleID}`);
-}) 
+})
 
 // create people function
 let warn = $("form[name=createPeople] .warmTips");
-createPeopleForm.addEventListener('submit',function(e){
+createPeopleForm.addEventListener('submit', function (e) {
   // prevent default refresh;
   e.preventDefault();
   let createName = $("form[name=createPeople] input[name=createName]").value;
   let createDOB = $("form[name=createPeople] input[name=createDOB]").value;
   let createAddress = $("form[name=createPeople] input[name=createAddress]").value;
   let createLicence = $("form[name=createPeople] input[name=createLicence]").value;
-  
-    // set and send post request;
-  if(createName == null || createLicence == null){
+
+  // set and send post request;
+  if (createName == null || createLicence == null) {
     warn.innerHTML = "Please fill the name and licence number";
-  }else{
+  } else {
     let xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("POST","./server/createPeople.php");
-    xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    xmlhttp.open("POST", "./server/createPeople.php");
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     console.log("xmlhttp create sucessful");
-    xmlhttp.onreadystatechange = function(){
-      if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+    xmlhttp.onreadystatechange = function () {
+      if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
         warn.innerText = xmlhttp.responseText;
       }
     }
     xmlhttp.send(`createName=${createName}&createDOB=${createDOB}&createAddress=${createAddress}&createLicence=${createLicence}`);
   }
-}) 
+})
 
 
 // create vehicles
@@ -269,20 +288,20 @@ console.log(warn);
 // let labelOwner = $("label[for=createOwner]");
 let ownerInfo = $("select.ownerInfo");
 console.log(ownerInfo);
-createCarContainer.addEventListener("mouseenter",e=>{
+createCarContainer.addEventListener("mouseenter", e => {
   console.log(e);
   let xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("GET","./server/createCar.php");
+  xmlhttp.open("GET", "./server/createCar.php");
   console.log("xmlhttp create sucessful");
-  xmlhttp.onreadystatechange = function(){
-    if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+  xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
       ownerInfo.innerHTML = xmlhttp.responseText;
     }
   }
   xmlhttp.send();
 })
 
-createCarForm.addEventListener('submit',function(e){
+createCarForm.addEventListener('submit', function (e) {
   // prevent default refresh;
   e.preventDefault();
   let createMake = $("form[name=createCar] input[name=createMake]").value;
@@ -292,16 +311,16 @@ createCarForm.addEventListener('submit',function(e){
   let ownerIndex = ownerInfo.selectedIndex;
   let createOwner = ownerInfo.options[ownerIndex].text;
   console.log(createOwner);
-    // set and send post request;
-  if(createLicence == null || createOwner == null){
+  // set and send post request;
+  if (createLicence == null || createOwner == null) {
     warntips.innerHTML = "Please fill the Licence and Owner";
-  }else{
+  } else {
     let xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("POST","./server/createCar.php");
-    xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    xmlhttp.open("POST", "./server/createCar.php");
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     console.log("xmlhttp create sucessful");
-    xmlhttp.onreadystatechange = function(){
-      if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+    xmlhttp.onreadystatechange = function () {
+      if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
         warntips.innerText = xmlhttp.responseText;
       }
     }
